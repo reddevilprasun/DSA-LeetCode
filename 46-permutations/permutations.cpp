2e008192ede36544e5d7b1ac246b66570567459b
PrasunKamilya <130187393+reddevilprasun@users.noreply.github.com>
2025-02-28 15:56:34 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (50.84%) - LeetSync

---
+++ 46-permutations/permutations.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void getAllPermute(vector<int>& arr, int idx,
+                       vector<vector<int>>& allPermute) {
+        if (idx == arr.size()) { // base case
+            allPermute.push_back({arr});
+            return;
+        }
+
+        for (int i = idx; i < arr.size(); i++) {
+            swap(arr[idx], arr[i]);
+            getAllPermute(arr, idx + 1, allPermute);
+            swap(arr[idx], arr[i]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> allPermute;
+        getAllPermute(nums, 0, allPermute);
+        return allPermute;
+    }
+};
