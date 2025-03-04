98d76acc87e0202aa44ed8d9a172efa33dd2d921
PrasunKamilya <130187393+reddevilprasun@users.noreply.github.com>
2025-03-04 21:46:10 +0530

    Time: 0 ms (100.00%) | Memory: 25.8 MB (93.68%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -2,19 +2,27 @@ class Solution {
 public:
     int trap(vector<int>& ht) {
         int n = ht.size();
-        vector<int> right(n);
-        vector<int> left(n);
-        right[n - 1] = ht[n - 1];
-        left[0] = ht[0];
-        for (int i = n - 2; i >= 0; i--) {
-            right[i] = max(right[i + 1], ht[i]); 
-        }
-        for (int i = 1; i < n; i++) {
-            left[i] = max(left[i - 1], ht[i]);
-        }
+        // vector<int> right(n);
+        // vector<int> left(n);
+        // right[n - 1] = ht[n - 1];
+        // left[0] = ht[0];
+        // for (int i = n - 2; i >= 0; i--) {
+        //     right[i] = max(right[i + 1], ht[i]); 
+        // }
+        // for (int i = 1; i < n; i++) {
+        //     left[i] = max(left[i - 1], ht[i]);
+        // }
+        int lmax = -1, rmax = -1;
+        int l = 0, r = n -1;
         int ans = 0;  
-        for(int i = 0 ; i < n ; i++) {
-            ans += min(left[i], right[i]) - ht[i];
+        while(l <r) {
+            lmax = max(lmax, ht[l]);
+            rmax = max(rmax,ht[r]);
+            if(lmax < rmax){
+                ans += lmax - ht[l++];
+            } else {
+                ans += rmax - ht[r--];
+            }
         }
         return ans;
     }
