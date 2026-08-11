b9ea97212db13696caf6d2d23f7b6f1e01667825
PrasunKamilya <130187393+reddevilprasun@users.noreply.github.com>
2026-08-11 17:31:34 +0530

    Time: 40 ms (89.96%), Space: 29 MB (92.71%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -4,9 +4,10 @@ public:
         int n = nums.size();
         sort(nums.begin(), nums.end());
         vector<vector<int>> ans;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n - 2; i++) {
             if (i > 0 && nums[i] == nums[i - 1])
                 continue;
+            if(nums[i] > 0) break;
             int j = i + 1, k = n - 1;
 
             while (j < k) {
